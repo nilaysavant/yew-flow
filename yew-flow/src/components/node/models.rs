16ba6ad5db65f3e6158b237c73ac83c537a020fa
Nilay Savant <nilayofficial@gmail.com>
2022-07-05 01:20:28 +0530

    WIP create `Edge` Model

--- yew-flow/src/components/node/models.rs
+++ yew-flow/src/components/node/models.rs
@@ -22,3 +22,19 @@ pub struct Node {
     pub inputs: Vec<NodeInput>,
     pub outputs: Vec<NodeOutput>,
 }
+
+/// Edge
+#[derive(Clone, PartialEq, Properties, Debug)]
+pub struct Edge {
+    pub id: usize,
+    /// From (x1, y1) coordinate
+    pub x1: u64,
+    /// From (x1, y1) coordinate
+    pub y1: u64,
+    /// To (x2, y2) coordinate
+    pub x2: u64,
+    /// To (x2, y2) coordinate
+    pub y2: u64,
+    pub color: Hsl,
+    pub is_active: bool,
+}
