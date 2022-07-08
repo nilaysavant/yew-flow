8fdad442337d4225e70a793ac0f4f0887ba3d6a4
Nilay Savant <nilayofficial@gmail.com>
2022-07-09 00:26:55 +0530

    WIP add viewport model

--- yew-flow/src/components/mod.rs
+++ yew-flow/src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod edge;
 pub mod node;
+pub mod viewport;
---
+++ yew-flow/src/components/viewport/models.rs
@@ -0,0 +1,9 @@
+use yew::prelude::*;
+
+/// # Yew Flow Viewport Model
+///
+/// Defines `yew-flow` viewport configuration
+pub struct Viewport {
+    /// Viewport element reference
+    pub reference: NodeRef,
+}
