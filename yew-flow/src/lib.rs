8dffd18724e9f2c6f94131b9d8ba3d1d7525ef64
Nilay Savant <nilayofficial@gmail.com>
2022-07-10 16:26:33 +0530

    Add standard unit type to standardise all types in the project

--- yew-flow/src/lib.rs
+++ yew-flow/src/lib.rs
@@ -2,5 +2,6 @@ pub mod components;
 pub mod constants;
 pub mod store;
 pub mod workspace;
+pub mod types;
 
 pub use workspace::Workspace;
---
+++ yew-flow/src/types/standard_unit.rs
@@ -0,0 +1,6 @@
+/// # Standard Unit
+///
+/// Standard unit/type for all metrics in the project.
+/// 
+/// - Currently set to f64, but can be changed centrally from here if needed.
+pub type StandardUnit = f64;
